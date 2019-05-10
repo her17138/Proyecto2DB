bcedc3e5953995925e769225d9c47ad4aad9beca
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-05-10 17:26:47 -0600

    UI agregar clientes

--- blog/resources/views/agregarClientes.blade.php
+++ blog/resources/views/agregarClientes.blade.php
@@ -4,6 +4,43 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>Ingreso de cliente</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+
+        <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="css/util.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    </head>
+    <body>
+        
+        <div class="container-contact100">
+        <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Creacion de tiendas</title>
 
         <!-- Fonts -->
@@ -31,51 +68,7 @@
         <link rel="stylesheet" type="text/css" href="css/util.css">
         <link rel="stylesheet" type="text/css" href="css/main.css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            #titulo{
-                font-family: 'Anton', sans-serif;
-                
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-        </style>
+       
     </head>
     <body>
         
@@ -89,12 +82,21 @@
             <form method="post" action="{{ route('producto.store') }}">
                 <div class="form-group">
                     @csrf
-                    <label for="id">Codigo del producto:</label>
-                    <input type="text" class="form-control" name="id"/>
+                    <div class="row">
+                        <div class="col">
+                            <label for="nombre">Nombre</label>
+                            <input type="text" class="form-control" placeholder="Nombre">
+                        </div>
+                        <div class="col">
+                            <label for="apellido">Apellido</label>
+                            <input type="text" class="form-control" placeholder="Apellido">
+                        </div>
+                    </div>
                 </div>
+                
                 <div class="form-group">
-                    <label for="name"> Nombre del producto :</label>
-                    <input type="text" class="form-control" name="nombre"/>
+                    <label for="direccion">Dirección</label>
+                    <input type="text" class="form-control" name="direccion"/>
                 </div>
                 <div class="form-group">
                     <label for="storeid">Id de la tienda:</label>
@@ -111,3 +113,5 @@
         </div>
     </body>
 </html>
+		
+            
